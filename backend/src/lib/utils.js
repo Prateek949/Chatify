f6659c89bc48de68e46e827f29fc8541d11d3584
Prateek Kaushik <prateekkaushik601@gmail.com>
2025-10-31 17:05:40 +0530

    Welcome Email Implemented

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,17 +1,18 @@
 import jwt from "jsonwebtoken";
+import { ENV } from "./env.js";
 
 
 export const generateToken = (userId,res) => {
-    const {JWT_SECRET,NODE_ENV}=process.env;
+    const {JWT_SECRET,NODE_ENV}=ENV;
     if(!JWT_SECRET) throw new Error("JWT_SECRET is not configured");
-    const token= jwt.sign({userId},process.env.JWT_SECRET,{
+    const token= jwt.sign({userId},ENV.JWT_SECRET,{
         expiresIn: "7d",
     });
     res.cookie("jwt",token,{
         maxAge: 7*24*60*60*1000, //ms
         httpOnly: true, //prevent XSS attacks: cross site scrioting
         sameSite: "strict",//csrf attacks
-        secure: process.env.NODE_ENV === "development"?false : true,
+        secure: ENV.NODE_ENV === "development"?false : true,
     });
     return token;
 };
