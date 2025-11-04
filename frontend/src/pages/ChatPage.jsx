03f7420d26299fb943a38d605f4ec368e41e7d26
Prateek Kaushik <prateekkaushik601@gmail.com>
2025-11-04 16:14:25 +0530

    frontend codebase setup

---
+++ frontend/src/pages/ChatPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useAuthStore } from '../store/useAuthStore';
+function ChatPage() {
+ 
+  return (
+    <div>ChatPage</div>
+  )
+}
+
+export default ChatPage
---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function SignUpPage() {
+     const {authUser,login,isLoggedIn} = useAuthStore();   
+  return (
+    <div>SignUpPage</div>
+  )
+}
+
+export default SignUpPage
