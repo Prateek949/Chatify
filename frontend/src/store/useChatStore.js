1d5b295867361d8fea1890197f341a53dd23ca7b
Prateek Kaushik <prateekkaushik601@gmail.com>
2025-11-06 15:54:04 +0530

    chat container setup done

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
-
+import toast from "react-hot-toast";
+import { axiosInstance } from "../lib/axios";
 
 export const useChatStore = create((set,get)=>({
     allContacts: [],
@@ -10,32 +11,52 @@ export const useChatStore = create((set,get)=>({
     isUserLoading: false,
     isMessageLoading:false,
     isSoundEnabled: JSON.parse(localStorage.getItem("isSoundEnabled")) === true,
+
+  
     toggleSound: () => {
         localStorage.setItem("isSoundEnabled", !get().isSoundEnabled)
         set({isSoundEnabled: !get().isSoundEnabled})
     },
+
     setActiveTab: (tab) => set({activeTab:tab}),
     setSelectedUser: (selectedUser) => set({selectedUser}),
+
+
     getAllContact: async () => {
-    set({ isUsersLoading: true });
+    set({ isUserLoading: true });
         try {
             const res = await axiosInstance.get("/messages/contacts");
             set({ allContacts: res.data });
         } catch (error) {
-            toast.error(error.response.data.message);
+          const errorMessage = error?.response?.data?.message || "Failed to fetch contacts.";
+          toast.error(errorMessage);
+          console.error("Error fetching contacts:", error); 
         } finally {
-             set({ isUsersLoading: false });
+             set({ isUserLoading: false });
      }
     },
     getMyChatPartners: async () => {
-    set({ isUsersLoading: true });
+    set({ isUserLoading: true });
     try {
       const res = await axiosInstance.get("/messages/chats");
       set({ chats: res.data });
     } catch (error) {
-      toast.error(error.response.data.message);
+      const errorMessage = error?.response?.data?.message || "Failed to fetch chats.";
+      toast.error(errorMessage);
+       console.error("Error fetching chat partners:", error); 
     } finally {
-      set({ isUsersLoading: false });
+      set({ isUserLoading: false });
     }
+    },
+    getMessagesByUserId: async (userId)=> {
+      set({ isMessageLoading : true})
+      try {
+        const res= await axiosInstance.get(`/messages/${userId}`);
+        set({message: res.data });
+      } catch (error) {
+        toast.error(error.response.data.message || "Something went wrong");
+      }finally{
+        set({isMessageLoading:false});
+      }
     },
 }))
